98d4336dd8d6162bbe9a6512cc852dc25d0e5a23
abiola <abiola.arobieke@gmail.com>
2023-05-02 15:13:54 -0400

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './style.css';
 import './index.html';
+import createNewElement from '../modules/render.js';
 
+const apiUrl = 'https://api.sampleapis.com/recipes/recipes';
+
+const getAllRecipe = async (url) => {
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const res = await response.json();
+  return res;
+};
+
+createNewElement(apiUrl, getAllRecipe);
