c971f4fc0c79a4651be8144c2c9f18dd8da6d69f
abiola <abiola.arobieke@gmail.com>
2023-05-03 06:34:36 -0400

    Add likes.js file to the project

---
+++ modules/likes.js
@@ -0,0 +1,28 @@
+export const addLike = async (url, itemObj) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: itemObj,
+  });
+  const res = await response;
+  if (res.status === 201) {
+    return true;
+  }
+  return false;
+};
+
+export const getLikes = async (url, id) => {
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const res = await response.json();
+
+  // Find number of likes for item_id
+  const itemLikes = res.find((item) => item.item_id === id);
+  return itemLikes ? itemLikes.likes : 0;
+};
