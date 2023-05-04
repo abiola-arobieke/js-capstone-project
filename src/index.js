f8799ea5fa247e1e62d62678ddcdfd89613f0929
emarkees <emarkees.com>
2023-05-04 10:05:10 -0600

    Update index.html files

--- src/index.js
+++ src/index.js
@@ -1,25 +1,16 @@
-import './style.css';
-import './index.html';
-import { apiUrl, likeUrl, commentUrl } from '../modules/endpoint.js';
-import createNewElement from '../modules/render.js';
-import { getAllLikes } from '../modules/likes.js';
-import {createPopup} from '../modules/popUp';
-
-
-createPopup();
-
-
+import './style.css’;
+import './index.html’;
+import { apiUrl, likeUrl } from '../modules/endpoint.js’;
+import createNewElement from '../modules/render.js’;
+import { getAllLikes } from '../modules/likes.js’;
 const getAllRecipe = async (url) => {
   const response = await fetch(url, {
-    method: 'GET',
+    method: 'GET’,
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type’: 'application/json’,
     },
   });
   const res = await response.json();
   return res;
 };
-
-createNewElement(apiUrl, likeUrl, getAllRecipe, getAllLikes );
-
-
+createNewElement(apiUrl, likeUrl, getAllRecipe, getAllLikes);
