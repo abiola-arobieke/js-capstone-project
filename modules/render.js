8cab6a383a8c7614a3325b8b9da10a469de864ef
emarkees <emarkees.com>
2023-05-04 10:01:34 -0600

    Update render.js files

--- modules/render.js
+++ modules/render.js
@@ -1,6 +1,5 @@
 import { addLike, getLikes } from './likes.js';
 import { likeUrl } from './endpoint.js';
-const popUp = document.getElementById('modal');
 const createNewElement = async (baseUrl, interUrl, recipeArray, likesArray) => {
   const totalRecipe = document.getElementById('recipe');
   const grid = document.getElementById('grid');
@@ -31,9 +30,49 @@ const createNewElement = async (baseUrl, interUrl, recipeArray, likesArray) => {
                   <button name=“${recipe.id}” class=“comment-btn py-10 bg-white capitalize” type=“button”>Comments</button>
               </div>
           </div>
+          <div class=“modal hide”>
+                        <div class=“modal-container”>
+                            <span id=“close” class=“close”>
+                                <i id=“${recipe.id} ${'close'}” class=“fa fa-times” aria-hidden=“true”></i>
+                            </span>
+                            <div class=“modal-info”>
+                                <div id=“recipe-info”>
+                                    <div id=“modal-detail”>
+                                        <div>
+                                            <img class=“w-100c pop-image” src=“${recipe.photoUrl}“>
+                                        </div>
+                                        <div class=“py-10”>
+                                            <h4 class=“ft-20 capitalize text-center”>${recipe.title}</h4>
+                                        </div>
+                                        <div class=“grid-2”>
+                                            <div class=“py-10">Calories: ${recipe.calories}</div>
+                                            <div class=“py-10”>Main Ingredient: ${recipe.mainIngredient}</div>
+                                            <div class=“py-10">Cholesterol: ${recipe.cholesterol}</div>
+                                            <div class=“py-10”>Cook Time: ${recipe.cookTime} mins</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class=“comments-section”>
+                                    <h4 class=“py-20 text-center”>Comment(2)</h4>
+                                    <ul>
+                                    <li>Alex is a boy</li>
+                                    <li>Alex is a boy</li>
+                                    <li>Alex is a boy</li>
+                                    </ul>
+                                </div>
+                                <div class=“w-100c”>
+                                    <h4 class=“form-text-header py-10 text-center”>Add a comment</h4>
+                                    <form  action=“form” class=“form d-flex flex-col gap-10”>
+                                    <input class=“py-10" type=“text” id=“userName” placeholder=“Your name” name=“user”>
+                                    <textarea name=“textarea” id=“textarea” cols=“30” rows=“10" placeholder=“Your insights”></textarea>
+                                    <button class=“py-10" type=“button” id=“comment-btn”>Comment</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                      </div>
         `;
     const divElement = document.createElement('div');
-    divElement.classList.add('card');
     divElement.innerHTML = cardChild;
     grid.appendChild(divElement);
     const likeBtn = document.getElementById(`${recipe.id}`);
@@ -52,13 +91,17 @@ const createNewElement = async (baseUrl, interUrl, recipeArray, likesArray) => {
         }
       }
     });
-    commentBtn.addEventListener('click', () => {
-      popUp.style.display = 'block';
+    commentBtn.addEventListener('click', (e) => {
+      const showModal = e.target.parentElement.parentElement.parentElement.lastElementChild;
+      showModal.classList.remove('hide');
+      showModal.classList.add('show');
+    });
+    const closeBtn = document.getElementById(`${recipe.id} ${'close'}`);
+    closeBtn.addEventListener('click', (e) => {
+      const closeModal = e.target.parentElement.parentElement.parentElement;
+      closeModal.classList.remove('show');
+      closeModal.classList.add('hide');
     });
   });
 };
-const closeBtn = document.getElementById('close');
-closeBtn.addEventListener('click', () => {
-  popUp.style.display = 'none';
-});
 export default createNewElement;
