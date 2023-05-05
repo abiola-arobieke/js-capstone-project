b982f0eaf00c9e31d21457dd604782e5b8eb1861
emarkees <emarkees.com>
2023-05-05 05:35:29 -0600

    Update endpoint.js files

---
+++ modules/endpoint.js
@@ -0,0 +1,5 @@
+export const apiUrl = 'https://api.sampleapis.com/recipes/recipes';
+export const likeUrl = `
+    https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Qcqa91MlwmRHZ16i7RQ5/likes`;
+export const commentUrl = `
+    https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Qcqa91MlwmRHZ16i7RQ5/comments`;
