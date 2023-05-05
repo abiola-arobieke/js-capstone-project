d72b5583867435270287232d9449784a309c6153
emarkees <emarkees.com>
2023-05-05 05:35:54 -0600

    Update comment.js files

---
+++ modules/comment.js
@@ -0,0 +1,25 @@
+export const addComment = async (url, userData) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: userData,
+  });
+  const res = await response;
+  if (res.status === 201) {
+    return true;
+  }
+  return false;
+};
+
+export const getComment = async (url) => {
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const res = await response.json();
+  return res;
+};
