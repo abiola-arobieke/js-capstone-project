f48f0afa3434f99cbd174fb456592d49ba9fa2e0
abiola <abiola.arobieke@gmail.com>
2023-05-07 09:39:47 -0400

    Modify index.js file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import './style.css';
 import './index.html';
-import { apiUrl, likeUrl } from '../modules/endpoint.js';
-import createNewElement from '../modules/render.js';
-import { getAllLikes } from '../modules/likes.js';
-// import { addComment } from '../modules/comment';
+import { apiUrl, likeUrl } from './modules/endpoint.js';
+import createNewElement from './modules/render.js';
+import { getAllLikes } from './modules/likes.js';
 
 const getAllRecipe = async (url) => {
   const response = await fetch(url, {
